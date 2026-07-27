cf04b3a65babd24caa6739b63ee9c70e61efa74e
Dixich-creator <dddddguuuu@yandex.ru>
2026-07-27 23:07:53 +0500

    Add files via upload

---
+++ auto_access.py
@@ -0,0 +1,52 @@
+from aiogram import Router
+from aiogram.types import ChatMemberUpdated
+from aiogram.enums import ChatMemberStatus
+
+from database import set_access
+
+
+router = Router()
+
+
+@router.my_chat_member()
+async def bot_added(event: ChatMemberUpdated):
+
+    # Проверяем, что бот добавили в группу
+
+    if event.new_chat_member.status not in [
+        ChatMemberStatus.MEMBER,
+        ChatMemberStatus.ADMINISTRATOR
+    ]:
+        return
+
+
+    chat = event.chat
+
+
+    try:
+
+        admins = await chat.get_administrators()
+
+    except Exception:
+
+        return
+
+
+
+    for admin in admins:
+
+        if admin.status == ChatMemberStatus.CREATOR:
+
+
+            await set_access(
+                admin.user.id,
+                4
+            )
+
+
+            print(
+                f"👑 {admin.user.full_name} получил 4 уровень"
+            )
+
+
+            break
