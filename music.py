3a6431eedd6b80fa0cd797a60a6c4e898fc50798
Dixich-creator <dddddguuuu@yandex.ru>
2026-07-27 21:56:30 +0500

    Update music.py

--- music.py
+++ music.py
@@ -1,16 +1,8 @@
-from aiogram import Router
-from aiogram.filters import Command
-from aiogram.types import Message, FSInputFile
-
-router = Router()
-print("🎵 MUSIC MODULE LOADED")
-
-
 @router.message(Command("music"))
 async def music(message: Message):
 
-    print("🎵 MUSIC COMMAND RECEIVED")
+    print("🎵 MUSIC COMMAND WORK")
 
     await message.answer(
-        "✅ Команда /music работает"
+        "🎵 Музыка работает!"
     )
