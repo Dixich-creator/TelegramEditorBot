ee35ec6b899ce4df32add2493747519646446dea
Dixich-creator <dddddguuuu@yandex.ru>
2026-07-27 21:58:29 +0500

    Update music.py

--- music.py
+++ music.py
@@ -1,8 +1,35 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message, FSInputFile
+
+router = Router()
+
+print("🎵 MUSIC MODULE LOADED")
+
+
 @router.message(Command("music"))
 async def music(message: Message):
 
     print("🎵 MUSIC COMMAND WORK")
 
-    await message.answer(
-        "🎵 Музыка работает!"
+    # отправляем кружок
+    video = FSInputFile(
+        "media/video.mp4"
+    )
+
+    await message.answer_video_note(
+        video_note=video
+    )
+
+
+    # отправляем песню
+    audio = FSInputFile(
+        "media/fluger.mp3"
+    )
+
+    await message.answer_audio(
+        audio=audio,
+        title="Fluger Music",
+        performer="Fluger New",
+        caption="🎵 Приятного прослушивания!"
     )
