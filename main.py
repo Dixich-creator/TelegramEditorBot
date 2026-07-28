7876a0e92f8717e0edd9c5af1d7db8586a7b6e55
Dixich-creator <dddddguuuu@yandex.ru>
2026-07-28 22:07:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ from roles import router as roles_router
 from shop import router as shop_router
 from fluger_shop import router as fluger_shop_router
 from fluger_inventory import router as fluger_inventory_router
+from fuse import router as fuse_router
 from buy import router as buy_router
 from open_case import router as open_case_router
 from inventory import router as inventory_router
@@ -77,6 +78,7 @@ async def main():
     dp.include_router(shop_router)
     dp.include_router(fluger_shop_router)
     dp.include_router(fluger_inventory_router)
+    dp.include_router(fuse_router)
     dp.include_router(buy_router)
     dp.include_router(open_case_router)
     dp.include_router(inventory_router)
