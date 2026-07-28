f6f5ade95bb0fb9aa49d1572c3f382c910874876
Dixich-creator <dddddguuuu@yandex.ru>
2026-07-28 12:32:31 +0500

    Update auto_access.py

--- auto_access.py
+++ auto_access.py
@@ -4,14 +4,11 @@ from aiogram.enums import ChatMemberStatus
 
 from database import add_user, set_access
 
-
 router = Router()
 
 
 @router.my_chat_member()
 async def bot_added(event: ChatMemberUpdated):
-
-    # Проверяем, что бот добавили в группу
 
     if event.new_chat_member.status not in [
         ChatMemberStatus.MEMBER,
@@ -19,37 +16,30 @@ async def bot_added(event: ChatMemberUpdated):
     ]:
         return
 
-
     chat = event.chat
 
-
     try:
-
         admins = await chat.get_administrators()
-
     except Exception:
-
         return
-
-
 
     for admin in admins:
 
         if admin.status == ChatMemberStatus.CREATOR:
 
-        await add_user(
-            admin.user.id,
-            admin.user.username,
-            admin.user.full_name
-        )
+            await add_user(
+                admin.user.id,
+                admin.user.username,
+                admin.user.full_name
+            )
 
-        await set_access(
-            admin.user.id,
-            4
-        )
+            await set_access(
+                admin.user.id,
+                4
+            )
 
-        print(
-            f"👑 {admin.user.full_name} получил 4 уровень"
-        )
+            print(
+                f"👑 {admin.user.full_name} получил 4 уровень"
+            )
 
-        break
+            break
